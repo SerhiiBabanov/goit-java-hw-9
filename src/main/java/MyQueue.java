e60eba9a4684082b4bc314011fafc5ec9cc21ae1
serhibabanov <QM5Hiu7dPvuRpyP>
2022-05-19 20:10:54 -0400

    create tests for MyQueue, change logic of methods

--- src/main/java/MyQueue.java
+++ src/main/java/MyQueue.java
@@ -1,11 +1,35 @@
-public class MyQueue<E> extends MyLinkedList<E> {
+import java.util.Queue;
+
+public class MyQueue<E> {
+    private MyLinkedList<E> myLinkedList;
+
+    public MyQueue() {
+        myLinkedList = new MyLinkedList();
+    }
+    public MyQueue add(E value){
+        myLinkedList.add(value);
+        return this;
+    }
+    public MyQueue remove(int index){
+        myLinkedList.remove(index);
+        return this;
+    }
+    public MyQueue clear(){
+        myLinkedList.clear();
+        return this;
+    }
+    public int size(){
+        return myLinkedList.size();
+    }
+
     public E peek(){
-        return headNode.data;
+        return myLinkedList.get(0);
     }
     public E pool(){
-        E result = headNode.data;
+        E result = myLinkedList.get(0);
         this.remove(0);
         return result;
     }
+
 }
 
--- src/test/java/MyQueueTest.java
+++ src/test/java/MyQueueTest.java
@@ -22,4 +22,31 @@ class MyQueueTest {
         assertEquals(testQueqe.peek(), 0, "must be 0");
         assertEquals(testQueqe.peek(), 0, "must be 1");
     }
+
+    @Test
+    void add() {
+        assertEquals(testQueqe.add(1).add(2).size(), 2);
+    }
+
+    @Test
+    void remove() {
+        assertEquals(testQueqe.add(0).add(1).add(2).add(3).size(), 4);
+        assertEquals(testQueqe.remove(0).size(),3);
+        assertEquals(testQueqe.remove(1).size(),2);
+        assertEquals(testQueqe.remove(1).size(),1);
+    }
+
+    @Test
+    void clear() {
+        assertEquals(testQueqe.add(0).add(1).size(), 2);
+        assertEquals(testQueqe.clear().size(), 0);
+    }
+
+    @Test
+    void size() {
+        assertEquals(testQueqe.add(0).add(1).size(), 2);
+        assertEquals(testQueqe.clear().size(), 0);
+
+    }
+
 }
