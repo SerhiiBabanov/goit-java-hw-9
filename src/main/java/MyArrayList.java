6cc826cc73e811a58c390d336dc848236e7663eb
serhiibabanov <serhii.babanov@gmail.com>
2022-05-22 12:40:31 -0400

    add javadoc to MyArrayList, MyLinkedList. Rename local variables

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,69 +1,109 @@
 import java.lang.reflect.Array;
 
 public class MyArrayList<E> {
-    private E[] arrayOfObject;
+    private E[] array;
     private final Class<E> clazz;
-    private int indexOfFirstEmptySpace;
+    private int pointer;
 
+    /**
+     * Create new object of MyArrayList.
+     * @param clazz  Class of storage objects.
+     */
     public MyArrayList(Class<E> clazz) {
-        this.arrayOfObject = (E[]) Array.newInstance(clazz, 10);
-        this.indexOfFirstEmptySpace = 0;
+        this.array = (E[]) Array.newInstance(clazz, 10);
+        this.pointer = 0;
         this.clazz = clazz;
     }
 
-    public MyArrayList(E[] innerArrayOfObject) {
-        this.arrayOfObject = innerArrayOfObject.clone();
-        this.indexOfFirstEmptySpace = innerArrayOfObject.length;
-        this.clazz = (Class<E>) innerArrayOfObject[0].getClass();
+    /**
+     * Create new object of MyArrayList.
+     * @param array Array of storage objects.
+     */
+    public MyArrayList(E[] array) {
+        this.array = array.clone();
+        this.pointer = array.length;
+        this.clazz = (Class<E>) array[0].getClass();
     }
 
+    /**
+     * add a new object to MyArrayList.
+     * @param value object for add.
+     * @return this MyArrayList
+     */
     public MyArrayList add(E value) {
-        if (arrayOfObject.length == indexOfFirstEmptySpace){
+        if (array.length == pointer){
             resize();
         }
-        arrayOfObject[indexOfFirstEmptySpace] = value;
-        indexOfFirstEmptySpace++;
+        array[pointer] = value;
+        pointer++;
         return this;
     }
+
+    /**
+     * remove an object from MyArrayList.
+     * @param index  index of object in the storage.
+     * @return this MyArrayList
+     */
     public MyArrayList remove(int index){
-        if (index == indexOfFirstEmptySpace-1){
-            arrayOfObject[index] = null;
-            indexOfFirstEmptySpace--;
+        //if index is bigger than count of element in array or index < 0
+        if (index >= pointer || index < 0 ){
+            throw new IndexOutOfBoundsException();
+        }
+        //if index is the last element in array of objects
+        if (index == pointer -1){
+            array[index] = null;
+            pointer--;
+            return this;
+        }
+        //if index from 0 to array.length - 2 (it is not the last element)
+        if (pointer - 1 - index >= 0) {
+            System.arraycopy(array, index + 1, array, index, pointer - 1 - index);
+            pointer--;
             return this;
         }
 
-        if (indexOfFirstEmptySpace - 1 - index >= 0) {
-            System.arraycopy(arrayOfObject, index + 1, arrayOfObject, index, indexOfFirstEmptySpace - 1 - index);
-            indexOfFirstEmptySpace--;
-            return this;
-        }
-        if (index >= indexOfFirstEmptySpace){
-            throw new IndexOutOfBoundsException();
-        }
         return this;
     }
+
+    /**
+     * delete all objects in the MyArrayList.
+     * @return this MyArrayList
+     */
     public MyArrayList clear() {
-        this.arrayOfObject = (E[]) Array.newInstance(clazz, 10);
-        this.indexOfFirstEmptySpace = 0;
+        this.array = (E[]) Array.newInstance(clazz, 10);
+        this.pointer = 0;
         return this;
     }
+
+    /**
+     * @return return count of objects in the MyArrayList.
+     */
     public int size() {
-        return indexOfFirstEmptySpace;
+        return pointer;
     }
+
+    /**
+     * @param index  index of object in the storage.
+     * @return element from the MyArrayList with index
+     */
     public E get(int index) {
         E result;
         try {
-            result = arrayOfObject[index];
+            result = array[index];
         } catch (IndexOutOfBoundsException e) {
             throw new RuntimeException("Index out of range of MyArrayList", e);
         }
         return result;
     }
+
+    /**
+     * Increase length of array for objects
+     */
     private void resize(){
-        int newSizeOfArrayOfObject = (int) (indexOfFirstEmptySpace * 1.5);
-        E[] newArrayOfObject = (E[]) Array.newInstance(clazz, newSizeOfArrayOfObject);
-        if (indexOfFirstEmptySpace >= 0)
-            System.arraycopy(arrayOfObject, 0, newArrayOfObject, 0, indexOfFirstEmptySpace);
-        arrayOfObject = newArrayOfObject;
+        int newSize = (int) (pointer * 1.5);
+        E[] newArray = (E[]) Array.newInstance(clazz, newSize);
+        if (pointer >= 0)
+            System.arraycopy(array, 0, newArray, 0, pointer);
+        array = newArray;
     }
 }
--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,72 +1,104 @@
 import java.util.Objects;
 
 public class MyLinkedList<E> {
-    protected Node headNode;
-    protected Node lastNode;
-    protected int countElement;
+    protected Node head;
+    protected Node end;
+    protected int countNodes;
 
+    /**
+     * Create new object of MyLinkedList.
+     */
     public MyLinkedList() {
-        this.headNode = null;
+        this.head = null;
     }
 
-
+    /**
+     * add a new object to MyLinkedList.
+     * @param value  object for add.
+     * @return this MyLinkedList.
+     */
     public MyLinkedList add(E value) {
         Node node = new Node();
         node.data = value;
-        if (Objects.isNull(headNode)) {
-            headNode = node;
-            lastNode = node;
-        } else {
-            lastNode.next = node;
-            node.previous = lastNode;
-            lastNode = node;
+        //if list is empty storage in head
+        if (Objects.isNull(head)) {
+            head = node;
+            end = node;
         }
-        countElement++;
+        // if list is not empty storage in the end
+        else {
+            end.next = node;
+            node.previous = end;
+            end = node;
+        }
+        countNodes++;
         return this;
     }
 
+    /**
+     * remove an object from MyLinkedList.
+     * @param index  index of object in the storage.
+     * @return this MyLinkedList
+     */
     public MyLinkedList remove(int index) {
-        Node currentNode = headNode;
+        Node currentNode = head;
+        //looks for an item with an index
         while (index != 0) {
             index--;
             currentNode = currentNode.next;
         }
-        if (currentNode == headNode) {
-            headNode = headNode.next;
-            countElement--;
-        } else if (currentNode == lastNode) {
-            lastNode = currentNode.previous;
-            countElement--;
-        } else {
+        // if the index = 0, delete the head
+        if (currentNode == head) {
+            head = head.next;
+            countNodes--;
+        }
+        // if the index = last element, delete the end
+        else if (currentNode == end) {
+            end = currentNode.previous;
+            countNodes--;
+        }
+        //if the index  is middle element
+        else {
             currentNode.previous.next = currentNode.next;
             currentNode.next.previous = currentNode.previous;
-            countElement--;
+            countNodes--;
         }
         return this;
     }
-
+    /**
+     * delete all objects in the MyLinkedList.
+     * @return this MyLinkedList
+     */
     public MyLinkedList clear() {
-        headNode = null;
-        lastNode = null;
-        countElement = 0;
+        head = null;
+        end = null;
+        countNodes = 0;
         return this;
     }
-
+    /**
+     * @return return count of objects in the MyLinkedList.
+     */
     public int size() {
 
-        return countElement;
+        return countNodes;
     }
-
+    /**
+     * @param index  index of object in the storage.
+     * @return element from the MyLinkedList with index
+     */
     public E get(int index) {
         Node currentNode;
-        if (((countElement - index) / (countElement * 1.0)) < 0.5) {
-            currentNode = lastNode;
-            while (index != countElement - 1) {
+        // if index more close to the end, looking from the end
+        if (((countNodes - index) / (countNodes * 1.0)) < 0.5) {
+            currentNode = end;
+            while (index != countNodes - 1) {
                 index++;
                 currentNode = currentNode.previous;
             }
-        } else {
-            currentNode = headNode;
+        }
+        // if index more close to the head, looking from the head
+        else {
+            currentNode = head;
             while (index != 0) {
                 index--;
                 currentNode = currentNode.next;
